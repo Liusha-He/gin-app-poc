e09f197500b0b89ad604d7f802b8a0379cd92b3d
lhe <liusha.8852@yahoo.com>
2024-02-04 00:20:52 +0000

    add more docs

--- src/api/accounts.go
+++ src/api/accounts.go
@@ -12,6 +12,15 @@ type createAccountRequest struct {
 	Currency string `json:"currency" binding:"required,oneof=USD EUR GBP CNY"`
 }
 
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type listAccountsRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
 // Create Account 	godoc
 // @Summary 		create account
 // @Schemes 		http
@@ -19,7 +28,7 @@ type createAccountRequest struct {
 // @Tags 			accounts
 // @Produce 		json
 // @Param 			account  body	createAccountRequest true  "account json"
-// @Success 		200
+// @Success 		200 {object} dao.Account
 // @Router 			/api/v1/accounts [post]
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
@@ -41,4 +50,57 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, account)
+}
+
+// GetAccountByID 	godoc
+// @Summary 		get account by id
+// @Schemes 		http
+// @Description 	Takes an account id with path, Returned account info json.
+// @Tags 			accounts
+// @Produce 		json
+// @Param 			id path string true "search by id"
+// @Success 		200	{object} dao.Account
+// @Router 			/api/v1/accounts/{id} [get]
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+// ListAccounts godoc
+// @Summary 		Get account list
+// @Schemes 		http
+// @Description		Responds with the list of accounts
+// @Tags 			accounts
+// @Produce 		json
+// @Success 		200	{array} dao.Account
+// @Router 			/api/v1/accounts [get]
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := dao.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
--- src/api/routers.go
+++ src/api/routers.go
@@ -4,4 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func (server *Server) AccountsRouters(r *gin.RouterGroup) {
 	r.POST("/accounts", server.createAccount)
+	r.GET("/accounts/:id", server.getAccount)
+	r.GET("/accounts", server.listAccounts)
 }
